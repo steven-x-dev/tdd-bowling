50c72d3fe33e64150fbbea28cd19c15a54e7e562
steven-x-dev <steven-dev@qq.com>
2020-09-14 19:48:21 +0800

    [steven] feat: implemented constructing a strike frame

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,19 +1,38 @@
 class Frame {
 
-    private int[] threw;
+    private int[] scores;
     private int currThrow;
+    private boolean throwComplete;
+    private int remainingScoringThrows;
 
     Frame() {
-        threw = new int[] { -1, -1, -1 } ;
+        scores = new int[] { -1, -1, -1 } ;
         currThrow = 0;
+        throwComplete = false;
+        remainingScoringThrows = 2;
     }
 
-    int[] getThrew() {
-        return threw;
+    int[] getScores() {
+        return scores;
     }
 
     int getCurrThrow() {
         return currThrow;
     }
 
+    boolean isThrowComplete() {
+        return throwComplete;
+    }
+
+    int getRemainingScoringThrows() {
+        return remainingScoringThrows;
+    }
+
+    void throwBall(int pinsDown) {
+        if (pinsDown == 10) {
+            scores[currThrow] = pinsDown;
+            currThrow++;
+            throwComplete = true;
+        }
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -6,8 +6,25 @@ class BowlingGameTest {
 
     @Test
     void should_construct_empty_frame() {
+
         Frame expected = new Frame();
+
         assertEquals(expected.getCurrThrow(), 0);
-        assertArrayEquals(expected.getThrew(), new int[] { -1, -1, -1 });
+        assertFalse(expected.isThrowComplete());
+        assertEquals(expected.getRemainingScoringThrows(), 2);
+        assertArrayEquals(expected.getScores(), new int[] { -1, -1, -1 });
+    }
+
+
+    @Test
+    void should_construct_strike_frame() {
+
+        Frame expected = new Frame();
+        expected.throwBall(10);
+
+        assertEquals(expected.getCurrThrow(), 1);
+        assertTrue(expected.isThrowComplete());
+        assertEquals(expected.getRemainingScoringThrows(), 2);
+        assertArrayEquals(expected.getScores(), new int[]{10, -1, -1});
     }
 }
