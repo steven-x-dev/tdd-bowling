b54d9bd923ea7a9128b3dc5e101a150dc0a0d3c9
steven-x-dev <steven-dev@qq.com>
2020-09-14 19:54:52 +0800

    [steven] feat: implemented constructing a spare frame

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,15 +1,22 @@
 class Frame {
 
+    private int remainingBalls;
     private int[] scores;
     private int currThrow;
     private boolean throwComplete;
     private int remainingScoringThrows;
 
+
     Frame() {
+        remainingBalls = 10;
         scores = new int[] { -1, -1, -1 } ;
         currThrow = 0;
         throwComplete = false;
         remainingScoringThrows = 2;
+    }
+
+    int getRemainingBalls() {
+        return remainingBalls;
     }
 
     int[] getScores() {
@@ -30,9 +37,18 @@ class Frame {
 
     void throwBall(int pinsDown) {
         if (pinsDown == 10) {
+            remainingBalls -= pinsDown;
             scores[currThrow] = pinsDown;
             currThrow++;
             throwComplete = true;
+        } else {
+            remainingBalls -= pinsDown;
+            scores[currThrow] = pinsDown;
+            currThrow++;
+            throwComplete = true;
+            if (currThrow > 1) {
+                remainingScoringThrows--;
+            }
         }
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -9,6 +9,7 @@ class BowlingGameTest {
 
         Frame expected = new Frame();
 
+        assertEquals(expected.getRemainingBalls(), 10);
         assertEquals(expected.getCurrThrow(), 0);
         assertFalse(expected.isThrowComplete());
         assertEquals(expected.getRemainingScoringThrows(), 2);
@@ -22,9 +23,25 @@ class BowlingGameTest {
         Frame expected = new Frame();
         expected.throwBall(10);
 
+        assertEquals(expected.getRemainingBalls(), 0);
         assertEquals(expected.getCurrThrow(), 1);
         assertTrue(expected.isThrowComplete());
         assertEquals(expected.getRemainingScoringThrows(), 2);
-        assertArrayEquals(expected.getScores(), new int[]{10, -1, -1});
+        assertArrayEquals(expected.getScores(), new int[]{ 10, -1, -1 });
+    }
+
+
+    @Test
+    void should_construct_spare_frame() {
+
+        Frame expected = new Frame();
+        expected.throwBall(8);
+        expected.throwBall(2);
+
+        assertEquals(expected.getRemainingBalls(), 0);
+        assertEquals(expected.getCurrThrow(), 2);
+        assertTrue(expected.isThrowComplete());
+        assertEquals(expected.getRemainingScoringThrows(), 1);
+        assertArrayEquals(expected.getScores(), new int[]{ 8, 2, -1 });
     }
 }
